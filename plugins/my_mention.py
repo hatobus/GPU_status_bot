8e1146fc369ec944f57bf4c3760d1ed9cc3b87c0
hatobus <s1240056@gmail.com>
2018-04-27 00:42:14 +0900

    [Update] Add cpustatus

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -5,6 +5,41 @@ import subprocess
 from prettytable import PrettyTable
 
 
+def grep_gpustatus(ip):
+    sshcmd = "ssh -o \"ConnectTimeout 1\" -oProxyCommand='ssh -W %h:%p gatelabo' "
+    #sshcmd = "ssh -oProxyCommand='ssh -W %h:%p gatelabo' "
+    cmd = sshcmd + ip + " \"nvidia-smi --query-gpu=utilization.gpu --format=csv, noheader, nounits\""
+    status = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    l = []
+    while True:
+        line = status.stdout.readline()
+        l.append(line.decode('utf-8'))
+        
+        if not line and status.poll() is not None:
+            break
+
+    print(l)
+    return l
+
+def grep_cpustatus(ip):
+    cpucmd = "mpstat"
+    status = subprocess.Popen(cpucmd, shell=True, stdout=subprocess.PIPE)
+    l = []
+
+    while True:
+        line = status.stdout.readline()
+        l.append(line.decode('utf-8'))
+
+        if not line and status.poll() is not None:
+            break
+
+    _cpustatus = l[-2].replace('\n', '')
+    cpustatus = _cpustatus.split()
+    cstat = cpustatus[-1]
+
+    return 100.0 - float(cstat)
+
+
 @respond_to('使用状況')
 def mention_func(message):
     with open("./iplist.txt", "r") as f:
@@ -13,30 +48,22 @@ def mention_func(message):
 
     print(IPs)
     gpustatus_dict = {}
-    for ip in IPs:
-        cmd = ""
-        sshcmd = "ssh -o \"ConnectTimeout 1\" -oProxyCommand='ssh -W %h:%p gatelabo' "
-        #sshcmd = "ssh -oProxyCommand='ssh -W %h:%p gatelabo' "
-        cmd = sshcmd + ip + " \"nvidia-smi --query-gpu=utilization.gpu --format=csv, noheader, nounits\""
-        status = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-        l = []
-        while True:
-            line = status.stdout.readline()
-            l.append(line.decode('utf-8'))
-            
-            if not line and status.poll() is not None:
-                break
-
-        print(l)
-        if l[0] == '':
-            l.append("Nan")
-        percentage = l[1].replace('\n', '')
-        gpustatus_dict[ip] = percentage
-
-    table = PrettyTable(['PC name', 'GPU status'])
+    
+    table = PrettyTable(['PC name', 'CPU status', 'GPU status'])
     table.padding_width = 1
+    
+    for ip in IPs:
 
-    for k, v in gpustatus_dict.items():
-        table.add_row([k, v])
+        gpuline = grep_gpustatus(ip)
+        cpustat = grep_cpustatus(ip)
+
+        cpustat = f"{cpustat:.2f}" + "%"
+
+        if gpuline[0] == '':
+            gpuline.append("Nan")
+        percentage = gpuline[1].replace('\n', '')
+        
+
+        table.add_row([ip, cpustat, percentage])
 
     message.channel.upload_content('GPUstatus.txt', table)
